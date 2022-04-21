0ce5b6e268afbf64b54b5c57fedfd566e460c9e7
Гость <Guest@iMac-Mac005-3.local>
2022-04-21 13:53:07 +0900

    first commit

---
+++ Anzliik/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  Anzliik
+//
+//  Created by Гость on 21.04.2022.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Anzliik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Anzliik
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Celleble: UILebel!
+    
+    override func viewDidLoad() {
+        super.awakeFromNib()()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected,animated: animated)
+        //
+        
+    }
+}
+
